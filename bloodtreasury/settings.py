3b9b56c1883037070d26fd273cb6b619d63a2476
vidhyasagarbobby <vidhyasagar732@gmail.com>
2021-05-03 13:55:59 +0530

    first version

--- bloodtreasury/settings.py
+++ bloodtreasury/settings.py
@@ -80,17 +80,26 @@ WSGI_APPLICATION = 'bloodtreasury.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'bloodtreasury',
+#         'USER': 'postgres',
+#         'PASSWORD': '1234',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432'
+#     }
+# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'bloodtreasury',
-        'USER': 'postgres',
-        'PASSWORD': '1234',
-        'HOST': '127.0.0.1',
+        'NAME': 'd35ee2fkg9o7da',
+        'USER': 'iqvxhfwqgybiop',
+        'PASSWORD': '9b268218a9f01b69e6752a61c8b89f7d4ea414b4b87e2fea90b7f44fb4e9c6c6',
+        'HOST': 'ec2-3-211-37-117.compute-1.amazonaws.com',
         'PORT': '5432'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
